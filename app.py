751defa7b23af1059b3df603396e9a0c78dafe49
jorgeescalantem <jorgan234@hotmail.com>
2024-07-09 15:15:50 -0500

    zc

--- app.py
+++ app.py
@@ -143,6 +143,7 @@ def enviar_mensajes_whatsapp(number):
     try:
         connection.request("POST","/v19.0/117168924654185/messages", data, headers)
         response = connection.getresponse()
+        respuesta=request.get_json()
 
         return jsonify({"status": response.status,"telefono":number,"reason":response.reason})
 
