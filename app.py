bd3323082207df9c766592e5f6b610401d98a4c3
jorgeescalantem <jorgan234@hotmail.com>
2024-07-22 00:18:15 -0500

    response method recibir

--- app.py
+++ app.py
@@ -155,8 +155,9 @@ def enviar_mensajes_whatsapp(number):
     try:
         connection.request("POST","/v19.0/117168924654185/messages", data, headers)
         response = connection.getresponse()
-        req = request.get_json()
-        recibir_mensajes(req)
+        #req = request.get_json()
+        #req=response.getheader()
+        recibir_mensajes(response)
         
         
         if response.status == 200:
