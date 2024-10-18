710b5b1ce82b39c0162d794eb271eb52df0aacba
jorgeescalantem <jorgan234@hotmail.com>
2024-10-18 14:31:32 -0500

    texp concatenate

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def enviar_mensajes_whatsapp(number):
             "interactive":{
                 "type":"button",
                 "body": {
-                    "text": "texto de prueba desde body"
+                    "text": "texto de prueba desde body"+texp+"TP"
                 },
                 "footer": {
                     "text": "Desea confirmar el servicio"
