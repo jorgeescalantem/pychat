caf5046ea68513d2fd3e98c2919a83f6f7e24804
jorgeescalantem <jorgan234@hotmail.com>
2024-10-17 19:10:44 -0500

    texto prueba desde body

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def enviar_mensajes_whatsapp(number):
             "interactive":{
                 "type":"button",
                 "body": {
-                    "text": textp
+                    "text": "texto de prueba desde body"
                 },
                 "footer": {
                     "text": "Desea confirmar el servicio"
