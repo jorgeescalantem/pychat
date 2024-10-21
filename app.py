93c89108ceb7a8e281a29bb1240ade7e00ba409d
jorgeescalantem <jorgan234@hotmail.com>
2024-10-21 16:55:35 -0500

    bearer p concatenate

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def enviar_mensajes_whatsapp(number):
     data=json.dumps(data)
     headers = {
         "Content-Type" : "application/json",
-        "Authorization" : TOKEN_P
+        "Authorization" : "Bearer"+" "+TOKEN_P
     }
 
     url = "https://graph.facebook.com/v20.0/117168924654185/messages"
